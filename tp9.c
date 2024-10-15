f8f2a6b36f994d41c650768d320a880fc5d13bee
arnaud rizzo <arnaud@rizzo.fr>
2024-10-15 14:06:08 +0200

    1

---
+++ tp9.c
@@ -0,0 +1,28 @@
+//librairi
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <time.h>
+
+//debut du programe
+int main(void){
+    //variable
+    int nombre_choisi=0;
+
+    printf("choisir un entier : ");
+    scanf("%d",&nombre_choisi);
+
+    for (int i = 0; i <= nombre_choisi; i++)
+    {
+        printf("la table de %d est :\n",i);
+        for (int j = 0; j < 10; j++){
+            printf("%d * %d = %d\n",j,i,j*i);
+        }          
+        
+        
+    }
+ 
+    return 0;//fini le programe et retourne que tous c'est bien passer
+    
+    
+}
