d4d64d36fb9338102da92604899dec87075ed4e9
arnaud rizzo <arnaud@rizzo.fr>
2024-10-15 12:10:46 +0200

    1

---
+++ tp8_tabledemulitiplication.c
@@ -0,0 +1,24 @@
+//librairi
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+//debut du programe
+int main(void){
+    
+    int nombre_choisi=0;
+
+    printf("choisir un entier : ");
+    scanf("%d",nombre_choisi);
+
+    for (int i = 0; i < 10; i++)
+    {
+        /* code */
+    }
+    
+    
+
+    return 0;//fini le programe et retourne que tous c'est bien passer
+    
+    
+}
