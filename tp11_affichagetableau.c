fe3f6a8acca9964d043a516afc1e00e642fca9ad
arnaud rizzo <arnaud@rizzo.fr>
2024-10-16 14:48:53 +0200

    1

---
+++ tp11_affichagetableau.c
@@ -0,0 +1,24 @@
+//librairi
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+//#include <time.h>
+
+
+//debut du programe
+int main(void){
+   //variable
+   int nombre;
+   int *adress_du_nombre;
+
+    nombre =4;
+
+    adress_du_nombre = &nombre;
+
+    *adress_du_nombre=2;
+
+   printf("le nombre vaut : %d\n",nombre);
+
+    return 0; //fini le programe et retourne que tous c'est bien passer
+    
+}
