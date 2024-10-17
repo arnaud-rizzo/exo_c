849d47fe64043948598c0b3f5a55fc9e77a6556b
arnaud rizzo <arnaud@rizzo.fr>
2024-10-17 11:34:47 +0200

    1

---
+++ exo_string1.c
@@ -0,0 +1,23 @@
+//librairi
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+//#include <time.h>
+
+
+
+
+
+//debut du programe
+int main(void){
+    //variable
+    char chaine[100];
+
+    //saisi de la chaine
+    printf("entrer une chaine : ");
+    scanf("%s",chaine);
+    printf("%s",chaine);
+
+    return 0; //fini le programe et retourne que tous c'est bien passer
+    
+}
