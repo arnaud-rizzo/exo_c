400775a3083892bba45c0fd44eb2328282b53693
arnaud rizzo <arnaud@rizzo.fr>
2024-10-18 10:59:44 +0200

    1

--- exo_struct.c
+++ exo_struct.c
@@ -16,9 +16,17 @@ typedef struct persone
 //debut du programe
 int main(void){
     //variable
-    Persone alain_de_loin ={"alain",30,1.8};
+    Persone alain_de_loin;
 
-    printf("%s a %d ans et mesure %f m",alain_de_loin.nom,alain_de_loin.age,alain_de_loin.taille);
+    printf("votre nom : ");
+    scanf("%s",&alain_de_loin.nom);
+    printf("votre age : ");
+    scanf("%d",&alain_de_loin.age);
+    printf("votre taille : ");
+    scanf("%f",&alain_de_loin.taille);
+    
+
+    printf("%s a %d ans et mesure %f m\n",alain_de_loin.nom,alain_de_loin.age,alain_de_loin.taille);
 
     return 0; //fini le programe et retourne que tous c'est bien passer
     
